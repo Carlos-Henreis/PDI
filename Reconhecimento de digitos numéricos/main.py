c388d9cb4d515ed311073bb0a17521349cc5ce08
Carlos Henrique Reis <carlos_henreis@unifei.edu.br>
2018-06-17 13:53:54 -0300

    main atualizada

--- Reconhecimento de digitos numéricos/main.py
+++ Reconhecimento de digitos numéricos/main.py
@@ -6,7 +6,8 @@ model_name = '0008_CH3F'
 ic = ImageClassifier()
 ic.set_classes(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 ic.set_model_name(model_name)
-
+ic.set_train_data_path('data/{}'.format(model_name))
+ic.train(2000)
 
 # use the model to classify images from a different folder
 ic.predict('data/{}/predict/'.format(model_name))
